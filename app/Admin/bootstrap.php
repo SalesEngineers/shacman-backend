5d0646e1648215b665cf98c54aafbe60855571ce
Ivan <prikot@live.ru>
2023-01-30 10:27:21 +0700

    js

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -19,3 +19,5 @@
  */
 
 Encore\Admin\Form::forget(['map', 'editor']);
+Encore\Admin\Admin::js('//api-maps.yandex.ru/2.1/?apikey=2b2f535a-5940-453e-b7a3-c723a9c1d0fe&lang=ru_RU');
+Encore\Admin\Admin::js(asset('js/map.js'));
