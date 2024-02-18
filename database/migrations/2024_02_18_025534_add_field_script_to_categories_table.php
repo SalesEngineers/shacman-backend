45e1c3d832447283e19d16e69360e095043d11c7
Ivan <prikot@live.ru>
2024-02-18 17:17:38 +0700

    fix

--- database/migrations/2024_02_18_025534_add_field_script_to_categories_table.php
+++ database/migrations/2024_02_18_025534_add_field_script_to_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFieldScriptToCategories extends Migration
+class AddFieldScriptToCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
